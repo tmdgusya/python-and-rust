25dd1d3b27b8b755c4678e70aada5d8f5a62e732
tmdgusya <dev0jsh@gmail.com>
2025-05-17 04:26:04 +0900

    variable and function

--- python/main.py
+++ python/main.py
@@ -1,5 +1,26 @@
+"""
+fn main() {
+    let x: f64 = 1.0;
+    let y: i32 = 10;
+    
+    println!("x = {}, y = {}", x, y);
+
+    let z: f64 = 1.2;
+    let y: i32 = z as i32;
+    println!("{} -> {}", z, y)
+}
+"""
+
 def main():
-    print("Hello from python!")
+    x = 1.0;
+    y = 10;
+    
+    print(f"x = {x}, y = {y}")
+    
+    z = 1.2;
+    y = int(z)
+    
+    print(f"z = {z}, y = {y}")
 
 
 if __name__ == "__main__":
--- rust_part/src/main.rs
+++ rust_part/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+fn add(num1: i32, num2: i32) -> i32 {
+    num1 + num2 // return 생략 가능함
 }
+
+fn swap(num1: i32, num2: i32) -> (i32, i32) {
+    (num2, num1)
+}
+
+fn main() {
+    let x: f64 = 1.0;
+    let y: i32 = 10;
+    
+    println!("x = {}, y = {}", x, y);
+
+    let z: f64 = 1.2;
+    let y: i32 = z as i32;
+    println!("{} -> {}", z, y);
+
+    println!("{}", add(1,2));
+
+    let (n1, n2) = swap(1, 2);
+    println!("{n1} {n2}");
+
+    let my_func = |x| x + 1;
+    println!("{}", my_func(3))
+}
+
